cd5ff3e235bd38bc5dc37dd6ffa9a80f4aad0859
Mark Baggett <mbagget1@utk.edu>
2018-11-05 15:32:36 -0500

    Add optional output directory parameter.

--- app/pdf_handler.py
+++ app/pdf_handler.py
@@ -2,11 +2,12 @@ from PyPDF2 import PdfFileWriter, PdfFileReader
 
 
 class PdfManipulator:
-    def __init__(self, filename):
+    def __init__(self, filename, output_directory="output_pdfs"):
         self.filename = filename
         self.contents = PdfFileReader(filename, "rb")
         self.output_file_name = ""
         self.output_pdf = PdfFileWriter()
+        self.output_directory = output_directory
 
     def has_a_cover_page(self):
         if self.contents.getPage(1).extractText().startswith("Accepted for the Council:"):
@@ -29,7 +30,7 @@ class PdfManipulator:
         return
 
     def write_output_to_disk(self):
-        with open(f"output_pdfs/{self.filename.split('/')[-1]}", "wb") as my_pdf:
+        with open(f"{self.output_directory}/{self.filename.split('/')[-1]}", "wb") as my_pdf:
             self.output_pdf.write(my_pdf)
         return
 
