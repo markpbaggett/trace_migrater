43b5bab28d63ced71d2695a8c399fbbd29d9ef2e
Mark Baggett <mbagget1@utk.edu>
2018-11-07 15:11:14 -0500

    Create EmbargoHandler class.

--- app/embargo_handler.py
+++ app/embargo_handler.py
@@ -2,7 +2,11 @@ import os
 import xmltodict
 import json
 import shutil
+from selenium.webdriver.chrome.options import Options
+from seleniumrequests import Chrome
+import yaml
 
+settings = yaml.load(open("config/config.yml", "r"))
 
 class EmbargoedFiles:
     def __init__(self, my_path: str, my_type: str="datastream"):
@@ -22,11 +26,17 @@ class EmbargoedFiles:
             with open(f"{self.path}{file}", 'r') as embargo:
                 text = embargo.read()
                 data = json.loads(json.dumps(xmltodict.parse(text)))
-                try:
-                    files_with_dates.append({"filename": file, "embargo-until": data["rdf:RDF"]["rdf:Description"][0]
-                    ['islandora-embargo:embargo-until']["#text"], "datastreams": self.get_list_of_dsids(data["rdf:RDF"]["rdf:Description"],file.replace(".xml", ''))},)
-                except KeyError:
-                    pass
+                if self.type == "datastream":
+                    try:
+                        files_with_dates.append({"filename": file, "embargo-until": data["rdf:RDF"]["rdf:Description"]
+                        [0]['islandora-embargo:embargo-until']["#text"], "datastreams": self.get_list_of_dsids(data["rdf:RDF"]["rdf:Description"],file.replace(".xml", ''))},)
+                    except KeyError:
+                        pass
+                else:
+                    try:
+                        files_with_dates.append({"filename": file, "embargo-until": data["rdf:RDF"]["rdf:Description"]["islandora-embargo:embargo-until"]["#text"], "datastreams": "all"})
+                    except:
+                        pass
         return files_with_dates
 
     @staticmethod
@@ -35,9 +45,49 @@ class EmbargoedFiles:
 
     def build_mods(self):
         for file in self.embargoed_files:
-            shutil.copy(f"{self.path.replace('rels-int/', 'mods/')}{file['filename']}", "test_mods")
+            shutil.copy(f"{self.path.replace('rels-ext/', 'mods/')}{file['filename']}", "test_mods_rels_ext")
+
+
+class EmbargoHandler:
+    def __init__(self, identifier):
+        self.identifier = identifier
+        self.options = Options()
+        self.options.add_argument("--headless")
+        self.driver = Chrome(executable_path=os.path.abspath("/usr/bin/chromedriver"), options=self.options)
+        self.setup_handler()
+
+    def setup_handler(self):
+        self.driver.get("https://trace.utk.edu/user/login")
+        username = self.driver.find_element_by_id("username")
+        password = self.driver.find_element_by_id("password")
+        username.send_keys(settings["user_name"])
+        password.send_keys(settings["password"])
+        self.driver.find_element_by_name("submit").click()
+        return
+
+    def get_embargo_until(self):
+        self.driver.get(self.identifier.replace("PDF", "RELS-INT"))
+        r = self.driver.request("GET", self.identifier.replace("PDF", "RELS-INT"))
+        data = json.loads(json.dumps(xmltodict.parse(r.content)))
+        print(data["rdf:RDF"]["rdf:Description"][0]['islandora-embargo:embargo-until']["#text"])
+        return
+
+    def download_pdf(self):
+        self.driver.get(self.identifier)
+        r = self.driver.request("GET", self.identifier)
+        if r.status_code == 200:
+            with open(f"{settings['for_dlshare']}/embargoed_files/{self.identifier.replace('https://trace.utk.edu/islandora/object/', '').replace('/datastream/PDF', '')}.pdf",
+                    "wb") as my_file:
+                my_file.write(r.content)
+        else:
+            print(r.status_code)
+            print(self.identifier)
+        return
 
 
 if __name__ == "__main__":
-    test = EmbargoedFiles("/home/mark/PycharmProjects/trace_unpublished/rels-int/")
-    test.build_mods()
+    # test = EmbargoedFiles("/home/mark/PycharmProjects/trace_unpublished/rels-int/")
+    # test.build_mods()
+    test = EmbargoHandler("https://trace.utk.edu/islandora/object/utk.ir.td:131/datastream/PDF")
+    test.get_embargo_until()
+    test.download_pdf()
